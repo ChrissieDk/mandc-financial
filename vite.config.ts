16b79ccb555293d465f03a20f5402e44869d3b9b
Christiaan de Kock <christiaan@getsavvi.co.za>
2024-09-30 11:15:43 +0200

    deployment updates

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from "autoprefixer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mandc-financial/",
+  // base: "/mandc-financial/",
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
